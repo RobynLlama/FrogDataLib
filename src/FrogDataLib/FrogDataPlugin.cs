d86d01063a9180851dd39ad8eede302ba1db6454
Robyn <rnixon98@gmail.com>
2026-02-10 02:59:53 -0600

    cleanup: remove tutorial text

--- src/FrogDataLib/FrogDataPlugin.cs
+++ src/FrogDataLib/FrogDataPlugin.cs
@@ -7,15 +7,6 @@ using HarmonyLib;
 
 namespace FrogDataLib;
 
-// Here are some basic resources on code style and naming conventions to help
-// you in your first CSharp plugin!
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
-
-// This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
-// NuGet package, and it will generate the BepInPlugin attribute for you!
-// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
 [BepInAutoPlugin]
 public partial class FrogDataPlugin : BaseUnityPlugin
 {
@@ -37,7 +28,5 @@ public partial class FrogDataPlugin : BaseUnityPlugin
     patcher.PatchAll(typeof(SaveManagerPatches));
 
     Log.LogInfo($"Patch count is {patcher.GetPatchedMethods().Count()}");
-
-
   }
 }
